dfad423d7531b9b1009ddb7324124e059c60b541
troy600 <bazartroy531@gmail.com>
2024-12-06 19:28:03 +0800

    script to run memz boot with sounds fixed bad code

--- memz.sh
+++ memz.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -audiodev pa,id=snd0 -machine pcspk-audiodev=snd0 -hda memz1m.qcow2 -hdd haha.img -m 1M -smp 4
+qemu-system-x86_64 -audiodev pa,id=snd0 -machine pcspk-audiodev=snd0 -hda memz.qcow2 -m 1M -smp 4
