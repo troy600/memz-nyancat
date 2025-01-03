0aed5c468972791a063820dbd482ae253e2ab762
troy600 <bazartroy531@gmail.com>
2025-01-03 16:17:38 +0800

    fix memz

--- memz.sh
+++ memz.sh
@@ -1,1 +1,2 @@
-qemu-system-x86_64 -audiodev pa,id=snd0 -machine pcspk-audiodev=snd0 -hda memz.qcow -m 1M -smp 4
+#qemu-system-x86_64 -enable-kvm -audiodev pa,id=snd0 -machine pcspk-audiodev=snd0 -hda memz.qcow -m 1M -smp 4
+qemu-system-x86_64  -audiodev pa,id=snd0 -machine pcspk-audiodev=snd0 -hda memz.qcow -m 1M -smp 4
